d0035a446c0e31906251733095e7a5009e3d1724
Stephen W <stwong1@gmail.com>
2017-05-20 08:31:43 -0700

    back button fix

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -17,13 +17,50 @@
         // Hides the title/log after a set time
         setTimeout(function () {
             $('#title').fadeOut('fast');
-        }, 2000); // time in milliseconds
+        }, 1000); // time in milliseconds
 
 
         $(document).ready(function () {
-            $('#student_page').click(function () {
+            $('#parent_icon').click(function () {
+                $('#login_page').hide();
+                $('#parent_page').show();
+                console.log("parent");
+            });
+
+            $('#student_icon').click(function () {
                 $('#login_page').hide();
                 $('#student_page').show();
+                console.log("student");
+            });
+
+            $('#teacher_icon').click(function () {
+                $('#login_page').hide();
+                $('#teacher_page').show();
+                console.log("teacher");
+            });
+
+            $('#back_icon').click(function () {
+                $('#teacher_page').hide();
+                $('#parent_page').hide();
+                $('#student_page').hide();
+                $('#login_page').show();
+                console.log("back_icon");
+            });
+
+            $('#back_icon2').click(function () {
+                $('#teacher_page').hide();
+                $('#parent_page').hide();
+                $('#student_page').hide();
+                $('#login_page').show();
+                console.log("back_icon");
+            });
+
+            $('#back_icon3').click(function () {
+                $('#teacher_page').hide();
+                $('#parent_page').hide();
+                $('#student_page').hide();
+                $('#login_page').show();
+                console.log("back_icon");
             });
         });
     }, {}] }, {}, [1]);
